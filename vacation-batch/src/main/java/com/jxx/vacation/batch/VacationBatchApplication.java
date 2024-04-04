c7d824597dbef68f858f74a4ec8512450c4d5c83
JxxHxxx <leesin5498@naver.com>
2024-04-05 08:47:15 +0900

    FEAT&TEST 배치 프로젝트 WAS 실행 환경 모니터링 로그 구축
    1. 스프링 컨텍스트 초기화가 완료될 시 아래 로그를 남긴다.
     - 애플리케이션 실행
     - 등록된 Job Bean
    
    2. 1분마다 컨텍스트 초기화 시점에 등록됨 Job Bean 이 활성화 되어 있는지 체크
    
    3. processor 부분 단위 테스크(실패/성공) 작성

--- vacation-batch/src/main/java/com/jxx/vacation/batch/VacationBatchApplication.java
+++ vacation-batch/src/main/java/com/jxx/vacation/batch/VacationBatchApplication.java
@@ -1,19 +1,72 @@
 package com.jxx.vacation.batch;
 
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
+@EnableScheduling
+@RequiredArgsConstructor
 @SpringBootApplication(scanBasePackages = {"com.jxx.vacation.core","com.jxx.vacation.batch"})
 public class VacationBatchApplication {
 
+    private final ApplicationContext context;
+    private static List<String> applicationRefreshAfterJobNames;
+
     public static void main(String[] args) {
         SpringApplication.run(VacationBatchApplication.class, args);
+    }
+
+    @EventListener(ContextRefreshedEvent.class)
+    public void init() {
+        // application Start
         log.info("\n=========================================" +
                 "\nVacation Batch App Start Success" +
                 "\nDevelop JxxHxx " +
                 "\n=========================================");
+
+        applicationRefreshAfterJobNames = getJobBeanNames();
+        List<String> jobBeanNames = getJobBeanNames();
+
+        for (String jobBeanName : jobBeanNames) {
+            log.info("\n=========================================" +
+                    "\nJob Name : {} " +
+                    "\nFirst Execution Time : ..." +
+                    "\nPeriod : ..." +
+                    "\n=========================================",jobBeanName);
+        }
+
+        log.info("\nTotal enrolled job : {}", jobBeanNames.size());
     }
 
+    @Scheduled(cron = "0 * * * * *")
+    public void checkActivateJob() {
+        BeanFactory beanFactory = context.getAutowireCapableBeanFactory();
+
+        StringBuilder jobActiveLog = new StringBuilder("\n=========================================");
+        for (String jobBeanName : applicationRefreshAfterJobNames) {
+            boolean isActive = beanFactory.containsBean(jobBeanName);
+            jobActiveLog.append("\njobBeanName : " + jobBeanName + " isActive : " + isActive);
+        }
+        jobActiveLog.append("\n=========================================");
+        log.info("{}", jobActiveLog);
+    }
+
+    private List<String> getJobBeanNames() {
+        return Arrays.stream(BeanFactoryUtils.beanNamesForTypeIncludingAncestors(context, Job.class))
+                .toList();
+    }
 }
---
+++ vacation-batch/src/test/java/com/jxx/vacation/batch/job/leave/processor/LeaveItemValidateProcessorTest.java
@@ -0,0 +1,88 @@
+package com.jxx.vacation.batch.job.leave.processor;
+
+import com.jxx.vacation.batch.job.leave.item.LeaveItem;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static com.jxx.vacation.core.vacation.domain.entity.VacationType.MORE_DAY;
+import static org.assertj.core.api.Assertions.*;
+
+
+class LeaveItemValidateProcessorTest {
+
+    private final LeaveItemValidateProcessor leaveItemValidateProcessor = new LeaveItemValidateProcessor();
+
+    @DisplayName("vacationStatus=ONGOING 이 아닐 때 " +
+            "LeaveItemValidateProcessor 를 실행할 경우 " +
+            "vacationStatus 는 ERROR 로 변경되고 DeductedAmount = 0 이다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"CREATE", "REJECT", "REQUEST","APPROVED" ,"CANCELED", "COMPLETED", "FAIL" ,"ERROR"})
+    void leave_item_validate_fail_cause_not_ongoing_vacation_status(String vacationStatus) throws Exception {
+        //given
+        LocalDateTime vacationStartDateTime = LocalDateTime.of(2025, 12, 12, 0, 0,0);
+        LocalDateTime vacationEndDateTime = LocalDateTime.of(2025, 12, 13, 0,0,0);
+        LeaveItem leaveItem = new LeaveItem(
+                "U00013",
+                true,
+                15f,
+                15f,
+                "이재헌",
+                "U00013",
+                2,
+                LocalDate.of(2023, 12, 12),
+                100l,
+                true,
+                vacationStatus,
+                MORE_DAY.name(),
+                vacationStartDateTime,
+                vacationEndDateTime,
+                "JXX",
+                "departId",
+                true);
+        //when
+        LeaveItem processedItem = leaveItemValidateProcessor.process(leaveItem);
+        //then
+        assertThat(processedItem.getVacationStatus()).isEqualTo("ERROR");
+        assertThat(processedItem.getDeductedAmount()).isEqualTo(0f);
+    }
+
+    @DisplayName("vacationStatus = ONGOING 일 때 " +
+            "LeaveItemValidateProcessor 를 실행할 경우" +
+            "vacationStatus = COMPLETED, DeductedAmount 는 휴가 일 수 만큼으로 변경된다.")
+    @Test
+    void leave_item_validate_success() throws Exception {
+        //given
+        String vacationStatus = "ONGOING";
+        LocalDateTime vacationStartDateTime = LocalDateTime.of(2025, 12, 12, 0, 0,0);
+        LocalDateTime vacationEndDateTime = LocalDateTime.of(2025, 12, 13, 0,0,0);
+        LeaveItem leaveItem = new LeaveItem(
+                "U00013",
+                true,
+                15f,
+                15f,
+                "이재헌",
+                "U00013",
+                2,
+                LocalDate.of(2023, 12, 12),
+                100l,
+                true,
+                vacationStatus,
+                MORE_DAY.name(),
+                vacationStartDateTime,
+                vacationEndDateTime,
+                "JXX",
+                "departId",
+                true);
+        //when
+        LeaveItem processedItem = leaveItemValidateProcessor.process(leaveItem);
+        //then
+        assertThat(processedItem.getVacationStatus()).isEqualTo("COMPLETED");
+        assertThat(processedItem.getDeductedAmount()).isEqualTo(2f);
+    }
+}
