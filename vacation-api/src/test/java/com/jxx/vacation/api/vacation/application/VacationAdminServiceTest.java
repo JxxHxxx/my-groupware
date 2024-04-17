6a0b795fe3c4c935478a5d1e1c0b6680c4690bbe
JxxHxxx <leesin5498@naver.com>
2024-04-17 15:39:31 +0900

    TEST 공동 연차 등록 API 단위 테스트 추가 중

--- vacation-api/src/test/java/com/jxx/vacation/api/vacation/application/VacationAdminServiceTest.java
+++ vacation-api/src/test/java/com/jxx/vacation/api/vacation/application/VacationAdminServiceTest.java
@@ -8,6 +8,7 @@ import com.jxx.vacation.api.vacation.dto.response.CommonVacationServiceResponse;
 import com.jxx.vacation.api.vacation.dto.response.VacationServiceResponse;
 import com.jxx.vacation.core.vacation.domain.entity.Vacation;
 import com.jxx.vacation.core.vacation.domain.entity.VacationDuration;
+import com.jxx.vacation.core.vacation.domain.entity.VacationStatus;
 import com.jxx.vacation.core.vacation.domain.exeception.VacationClientException;
 import com.jxx.vacation.core.vacation.infra.VacationDurationRepository;
 import com.jxx.vacation.core.vacation.infra.VacationRepository;
@@ -33,25 +34,30 @@ class VacationAdminServiceTest {
     @Autowired
     VacationDurationRepository vacationDurationRepository;
 
-    @DisplayName("테스트")
+    @DisplayName("공동 연차 등록 정상 케이스" +
+            "공동 연차 글로벌 정책 " +
+            "1. 하루 단위로만 등록 가능하다." +
+            "2. 결재 여부, 개인 연차에서 차감 여부를 결정해야 한다." +
+            "3. 결재 X, 차감 O 라면 선차감되어 API 정상 응답 시, 바로 사용자의 잔여 연차가 감소한다." +
+            "4. 결재 O, 차감 O 의 경우 개인 연차 신청 API와 동일한 플로우를 탄다.")
     @Test
-    void test_success_case() {
+    void assign_common_vacation_success_case() {
         //given
         UserSession userSession = new UserSession("JXX", "제이주식회사", "U00001", "이재헌", "J00001", "IT센터");
         LocalDate vacationDate = LocalDate.of(2024, 4, 17);
         CommonVacationForm form = new CommonVacationForm("JXX", false, false, List.of(vacationDate));
         CommonVacationServiceForm serviceForm = new CommonVacationServiceForm(userSession, form);
-
         //when
         CommonVacationServiceResponse response = vacationAdminService.assignCommonVacation(serviceForm);
-
+        //then
         VacationServiceResponse vacationServiceResponse = response.vacations().get(0);
         assertThat(vacationServiceResponse.vacationDuration())
                 .extracting("startDateTime").containsExactly(vacationDate.atStartOfDay());
 
         assertThat(vacationServiceResponse.vacationDuration())
                 .extracting("endDateTime").containsExactly(vacationDate.atTime(23, 59, 59));
-        //then
+
+        assertThat(vacationServiceResponse.vacationStatus()).isEqualTo(VacationStatus.NON_REQUIRED);
     }
 
     @DisplayName("동일한 요일을 공동 연차로 중복 신청할 시, VacationClientException 발생한다.")
