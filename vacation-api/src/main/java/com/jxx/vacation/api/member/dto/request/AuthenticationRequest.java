fd24d97c28c164f72bac6857f3e1e711579fa0d4
JxxHxxx <leesin5498@naver.com>
2024-05-14 17:29:31 +0900

    FEAT 클라이언트 검증시 필요 필드 추가

--- vacation-api/src/main/java/com/jxx/vacation/api/member/dto/request/AuthenticationRequest.java
+++ vacation-api/src/main/java/com/jxx/vacation/api/member/dto/request/AuthenticationRequest.java
@@ -3,6 +3,9 @@ package com.jxx.vacation.api.member.dto.request;
 public record AuthenticationRequest(
         String memberId,
         String companyId,
-        String departmentId
+        String departmentId,
+        String name,
+        String companyName,
+        String departmentName
 ) {
 }
