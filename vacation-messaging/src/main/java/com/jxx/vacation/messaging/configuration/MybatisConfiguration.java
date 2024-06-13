480362ef0e886ddee03e47cd26494968fc58928a
JxxHxxx <leesin5498@naver.com>
2024-06-13 15:23:32 +0900

    CONFIG SQL 로그 설정 및 Mybatis 설정
    1. 복수 개의 DataSource 로 인해 Mybatis 추가 구성
    - @MapperScan 으로 스캔할 Mapper 인터페이스 한정
    - Mybatis Mapper 인터페이스가 바라볼 Datasource 지정
    
    추후 다른 DB에 연결한다면 추가 구성이 필요함

---
+++ vacation-messaging/src/main/java/com/jxx/vacation/messaging/configuration/MybatisConfiguration.java
@@ -0,0 +1,36 @@
+package com.jxx.vacation.messaging.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan(value="com.jxx.vacation.messaging.infra.mapper")
+@RequiredArgsConstructor
+public class MybatisConfiguration {
+
+    @Qualifier("approvalDataSource")
+    private final DataSource dataSource;
+    private final ApplicationContext applicationContext;
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(@Qualifier("approvalDataSource") DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        // yml 파일 설정 값을 인지 못하는건지 아래처럼 세팅해줘야 XML 파일을 인지함
+        factoryBean.setMapperLocations(applicationContext.getResources("classpath:mapper/*.xml"));
+        return factoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate() throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory(dataSource));
+    }
+}
