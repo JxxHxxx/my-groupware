a7b567418756d2d2b6c26341c30f91496b630f07
JxxHxxx <leesin5498@naver.com>
2025-04-28 16:40:10 +0900

    TEST - QueryBuilder insert 메서드 단위 테스트 작성

--- groupware-messaging/src/main/java/com/jxx/groupware/messaging/application/sql/validate/SimpleSqlQueryValidator.java
+++ groupware-messaging/src/main/java/com/jxx/groupware/messaging/application/sql/validate/SimpleSqlQueryValidator.java
@@ -17,11 +17,13 @@ public class SimpleSqlQueryValidator implements SqlQueryValidator {
         List<String> columnNames = parameter.columnNames();
         Set<String> requestParamKeySet = parameter.requestParamKeySet();
 
+        // 사이즈가 일치하는지
         if (!Objects.equals(columnNames.size(), requestParamKeySet.size())) {
             log.info("컬럼매핑 수와 요청 받은 파라미터 keySet 의 수가 일치하지 않습니다. \ncolumnName: {} requestParamKeySet:{}", columnNames, requestParamKeySet);
             return true;
         }
 
+        // 사이즈가 일치하면서 모든 값이 동일한지
         if (!requestParamKeySet.containsAll(columnNames)) {
             log.error("요청 파라미터 KeySet 에 포함되지 않은 컬림매핑이 존재합니다 \ncolumnName: {} requestParamKeySet:{}", columnNames, requestParamKeySet);
             return true;
--- groupware-messaging/src/test/java/com/jxx/groupware/messaging/application/sql/builder/SimpleSqlQueryBuilderTest.java
+++ groupware-messaging/src/test/java/com/jxx/groupware/messaging/application/sql/builder/SimpleSqlQueryBuilderTest.java
@@ -1,6 +1,7 @@
 package com.jxx.groupware.messaging.application.sql.builder;
 
 
+import com.jxx.groupware.core.messaging.domain.UnProcessableException;
 import com.jxx.groupware.messaging.application.sql.validate.SimpleSqlQueryValidator;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
@@ -20,6 +21,73 @@ class SimpleSqlQueryBuilderTest {
 
     public SimpleSqlQueryBuilderTest() {
         this.sqlQueryBuilder = new SimpleSqlQueryBuilder(new SimpleSqlQueryValidator());
+    }
+
+    @DisplayName("requestParams 의 keySet 과 columnNames 리스트는 동일해야 합니다. " +
+            "다시 말해 columnNames 의 모든 값들과 requestParams keySet 모든 값들은 일치해야 합니다.")
+    @Test
+    void insert_success_case() {
+        String tableName = "NOTIFICATION_TEST";
+
+        List<String> columnNames = List.of("MEMBER_ID", "WEIGHT", "CONTENT", "AGE");
+
+        Map<String, String> requestParams = new HashMap<>();
+        requestParams.put("CONTENT", "개인정보");
+        requestParams.put("AGE", "30");
+        requestParams.put("WEIGHT", "29.3");
+        requestParams.put("MEMBER_ID", "JxxHxxx"); // columnNames 등록되지 않은 정보
+
+        InsertBuilderParameter parameter = new InsertBuilderParameter(tableName, columnNames, requestParams);
+
+        assertThatCode(() -> sqlQueryBuilder.insert(parameter))
+                .doesNotThrowAnyException();
+
+        // 로그 출력을 위한 편의 테스트
+        String insertQuery = sqlQueryBuilder.insert(parameter);
+        log.info("insertQuery {}", insertQuery);
+    }
+
+    @DisplayName("requestParams key 들과 columnNames 값들은 동일해야 한다. " +
+            "1. requestParams 에만 존재하는 값이 있을 경우" +
+            "UnProcessableException 예외가 발생한다.")
+    @Test
+    void insert_fail_case1() {
+        String tableName = "NOTIFICATION_TEST";
+
+        List<String> columnNames = List.of("MEMBER_ID", "WEIGHT", "CONTENT", "AGE");
+
+        Map<String, String> requestParams = new HashMap<>();
+        requestParams.put("CONTENT", "개인정보");
+        requestParams.put("AGE", "30");
+        requestParams.put("WEIGHT", "29.3");
+        requestParams.put("MEMBER_ID", "JxxHxxx");
+        requestParams.put("ORG_NAME", "IT Service"); // columnNames 등록되지 않은 key
+
+        InsertBuilderParameter parameter = new InsertBuilderParameter(tableName, columnNames, requestParams);
+
+        Assertions.assertThatThrownBy(() -> sqlQueryBuilder.insert(parameter))
+                .isInstanceOf(UnProcessableException.class);
+    }
+
+    @DisplayName("requestParams key 들과 columnNames 값들은 동일해야 한다. " +
+            "2. columnNames 에만 존재하는 값이 있을 경우" +
+            "UnProcessableException 예외가 발생한다.")
+    @Test
+    void insert_fail_case2() {
+        String tableName = "NOTIFICATION_TEST";
+
+        List<String> columnNames = List.of("MEMBER_ID", "WEIGHT", "CONTENT", "AGE");
+
+        // MEMBER_ID 누락
+        Map<String, String> requestParams = new HashMap<>();
+        requestParams.put("CONTENT", "개인정보");
+        requestParams.put("AGE", "30");
+        requestParams.put("WEIGHT", "29.3");
+
+        InsertBuilderParameter parameter = new InsertBuilderParameter(tableName, columnNames, requestParams);
+
+        Assertions.assertThatThrownBy(() -> sqlQueryBuilder.insert(parameter))
+                .isInstanceOf(UnProcessableException.class);
     }
 
     @DisplayName("UPDATE Query 문을 만드는 로직을 테스트 한다." +
@@ -88,11 +156,5 @@ class SimpleSqlQueryBuilderTest {
 
         assertThatThrownBy(() -> sqlQueryBuilder.update(parameter))
                 .isInstanceOf(RdbMessagePolicyException.class);
-    }
-
-    @Test
-    void insert() {
-//        InsertBuilderParameter parameter = new InsertBuilderParameter();
-//        sqlQueryBuilder.insert(parameter);
     }
 }
